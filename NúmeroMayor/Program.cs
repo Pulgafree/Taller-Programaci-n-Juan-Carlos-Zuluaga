5845163b3d5cba486d0fc11c57c6c822d35490e2
Emiliano Barrionuevo <pulgafree@gmail.com>
2015-08-07 16:14:20 -0300

    Agregado Proyecto Número Mayor

---
+++ NúmeroMayor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NúmeroMayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primerN, segundoN, tercerN;
+            do
+            {
+                Console.Write("Ingrese el primer número: ");
+                primerN = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingrese el segundo número: ");
+                segundoN = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingrese el tercer número: ");
+                tercerN = Convert.ToInt32(Console.ReadLine());
+
+                if (primerN > segundoN && primerN > tercerN)
+                {
+                    Console.WriteLine("El número mayor es {0}", primerN);
+                }
+
+                if (segundoN > primerN && segundoN > tercerN)
+                {
+                    Console.WriteLine("El número mayor es {0}", segundoN);
+                }
+
+                if (tercerN > primerN && tercerN > segundoN)
+                {
+                    Console.WriteLine("El número mayor es {0}", tercerN);
+                }
+            } while (true);
+        }
+    }
+}
