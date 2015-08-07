9b2b38aef97fcef681fa48519de7db587d590ac7
Emiliano Barrionuevo <pulgafree@gmail.com>
2015-08-07 15:58:34 -0300

    Agregado Proyecto Numero es par o no

---
+++ NumeroPar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NumeroPar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            do
+            {
+                Console.Write("Ingrese número: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                if (numero % 2 == 0) // El número es par
+                {
+                    Console.WriteLine("El número: {0}, es par.", numero);
+                }
+                else
+                {
+                    Console.WriteLine("El número: {0}, no es par.", numero);
+                }
+            } while (true);
+        }
+    }
+}
